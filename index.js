c3393d07cbc79e59e3f99c2ef424027e9e8d39ea
Randy Herasme <randyherasme6@gmail.com>
2018-07-19 04:31:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,12 @@ function theBeatlesPlay(musicians,intruments) {
 }
 return array
 }
-
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
 
 function johnLennonFacts(facts){
   var array=[]
